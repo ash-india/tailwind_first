c41f0c0b88d05c4891ee364a23283b7bc14a7145
ash-india <niteshmodi00007@gmail.com>
2023-10-19 16:06:22 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./distribution/*.html"],   //all the html files will be handled from here, when '*' is there, otherwise index.html
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
